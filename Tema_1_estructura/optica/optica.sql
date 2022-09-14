125690ab7fec7462af587741260684e96c5d9c41
Newbiecodeweaver <carlosgilino@gmail.com>
2022-09-14 10:09:32 +0200

    optica msql structure updated

--- Tema_1_estructura/optica/optica.sql
+++ Tema_1_estructura/optica/optica.sql
@@ -5,9 +5,315 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+-- -----------------------------------------------------
+-- Schema pizzeria
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema pizzeria
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `pizzeria` DEFAULT CHARACTER SET utf8 ;
+-- -----------------------------------------------------
+-- Schema optica
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
 -- Schema optica
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `optica` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`lente_izquierda`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`lente_izquierda` (
+  `id_lente_izquierda` INT NOT NULL,
+  `graduacion` VARCHAR(45) NOT NULL,
+  `color` VARCHAR(12) NOT NULL,
+  PRIMARY KEY (`id_lente_izquierda`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`lente_derecha`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`lente_derecha` (
+  `id_lente_derecha` INT NOT NULL,
+  `graduacion` VARCHAR(45) NOT NULL,
+  `color` VARCHAR(12) NOT NULL,
+  PRIMARY KEY (`id_lente_derecha`))
+ENGINE = InnoDB;
+
+USE `pizzeria` ;
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`categoria_pizza`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pizzeria`.`categoria_pizza` (
+  `id_categoria_pizza` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `Nombre` ENUM('especial', 'de la casa', 'normal') NOT NULL,
+  `fecha_inicio` DATE NULL DEFAULT NULL,
+  `fecha_fin` DATE NULL DEFAULT NULL,
+  PRIMARY KEY (`id_categoria_pizza`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`provincia`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pizzeria`.`provincia` (
+  `id_provincia` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(15) NOT NULL,
+  PRIMARY KEY (`id_provincia`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`localidad`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pizzeria`.`localidad` (
+  `id_localidad` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(15) NOT NULL,
+  `provincia_id_provincia` INT(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id_localidad`),
+  INDEX `fk_localidad_provincia1_idx` (`provincia_id_provincia` ASC) ,
+  CONSTRAINT `fk_localidad_provincia1`
+    FOREIGN KEY (`provincia_id_provincia`)
+    REFERENCES `pizzeria`.`provincia` (`id_provincia`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`cliente`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pizzeria`.`cliente` (
+  `id_cliente` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(20) NOT NULL,
+  `apellidos` VARCHAR(20) NOT NULL,
+  `direccion` VARCHAR(25) NOT NULL,
+  `codigo_postal` INT(5) NOT NULL,
+  `numero_telefono` INT(9) NOT NULL,
+  `localidad_id_localidad` INT(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id_cliente`),
+  INDEX `fk_cliente_localidad1_idx` (`localidad_id_localidad` ASC) ,
+  CONSTRAINT `fk_cliente_localidad1`
+    FOREIGN KEY (`localidad_id_localidad`)
+    REFERENCES `pizzeria`.`localidad` (`id_localidad`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`tienda`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pizzeria`.`tienda` (
+  `id_tienda` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `direccion` VARCHAR(20) NOT NULL,
+  `codigo_postal` INT(5) NOT NULL,
+  `localidad_id_localidad` INT(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id_tienda`),
+  INDEX `fk_tienda_localidad1_idx` (`localidad_id_localidad` ASC) ,
+  CONSTRAINT `fk_tienda_localidad1`
+    FOREIGN KEY (`localidad_id_localidad`)
+    REFERENCES `pizzeria`.`localidad` (`id_localidad`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`empleado`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pizzeria`.`empleado` (
+  `id_empleado` INT(11) NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(20) NOT NULL,
+  `apellidos` VARCHAR(20) NOT NULL,
+  `nif` VARCHAR(9) NOT NULL,
+  `telefono` INT(9) NOT NULL,
+  `tipo_empleado` ENUM('cocinero', 'repartidor') NOT NULL,
+  `tienda_id_tienda` INT(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id_empleado`),
+  INDEX `fk_empleado_tienda1_idx` (`tienda_id_tienda` ASC) ,
+  CONSTRAINT `fk_empleado_tienda1`
+    FOREIGN KEY (`tienda_id_tienda`)
+    REFERENCES `pizzeria`.`tienda` (`id_tienda`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`comanda`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pizzeria`.`comanda` (
+  `id_comanda` INT(11) NOT NULL AUTO_INCREMENT,
+  `fecha_hora` DATETIME NOT NULL,
+  `forma_entrega` ENUM('restaurante', 'domicilio') NOT NULL,
+  `precio_total` DOUBLE NULL DEFAULT NULL,
+  `fecha_entrega` DATETIME NULL DEFAULT NULL,
+  `cliente_id_cliente` INT(10) UNSIGNED NOT NULL,
+  `tienda_id_tienda` INT(10) UNSIGNED NOT NULL,
+  `empleado_id_empleado` INT(11) NOT NULL,
+  PRIMARY KEY (`id_comanda`),
+  INDEX `fk_comanda_cliente1_idx` (`cliente_id_cliente` ASC) ,
+  INDEX `fk_comanda_tienda1_idx` (`tienda_id_tienda` ASC) ,
+  INDEX `fk_comanda_empleado1_idx` (`empleado_id_empleado` ASC) ,
+  CONSTRAINT `fk_comanda_cliente1`
+    FOREIGN KEY (`cliente_id_cliente`)
+    REFERENCES `pizzeria`.`cliente` (`id_cliente`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comanda_empleado1`
+    FOREIGN KEY (`empleado_id_empleado`)
+    REFERENCES `pizzeria`.`empleado` (`id_empleado`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comanda_tienda1`
+    FOREIGN KEY (`tienda_id_tienda`)
+    REFERENCES `pizzeria`.`tienda` (`id_tienda`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`producto_bebida`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pizzeria`.`producto_bebida` (
+  `id_producto_bedida` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(30) NOT NULL,
+  `descripcion` MEDIUMTEXT NOT NULL,
+  `imagen` VARCHAR(20) NOT NULL,
+  `precio` DOUBLE NOT NULL,
+  PRIMARY KEY (`id_producto_bedida`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`comanda_has_producto_bebida`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pizzeria`.`comanda_has_producto_bebida` (
+  `comanda_id_comanda` INT(11) NOT NULL,
+  `producto_bebida_id_producto_bedida` INT(10) UNSIGNED NOT NULL,
+  `cantidad` INT(2) NULL DEFAULT NULL,
+  PRIMARY KEY (`comanda_id_comanda`, `producto_bebida_id_producto_bedida`),
+  INDEX `fk_comanda_has_producto_bebida_producto_bebida1_idx` (`producto_bebida_id_producto_bedida` ASC) ,
+  INDEX `fk_comanda_has_producto_bebida_comanda1_idx` (`comanda_id_comanda` ASC) ,
+  CONSTRAINT `fk_comanda_has_producto_bebida_comanda1`
+    FOREIGN KEY (`comanda_id_comanda`)
+    REFERENCES `pizzeria`.`comanda` (`id_comanda`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comanda_has_producto_bebida_producto_bebida1`
+    FOREIGN KEY (`producto_bebida_id_producto_bedida`)
+    REFERENCES `pizzeria`.`producto_bebida` (`id_producto_bedida`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`producto_hamburguesa`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pizzeria`.`producto_hamburguesa` (
+  `id_producto_hamburguesa` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(30) NOT NULL,
+  `descripcion` MEDIUMTEXT NOT NULL,
+  `imagen` VARCHAR(20) NOT NULL,
+  `precio` DOUBLE NOT NULL,
+  PRIMARY KEY (`id_producto_hamburguesa`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`comanda_has_producto_hamburguesa`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pizzeria`.`comanda_has_producto_hamburguesa` (
+  `comanda_id_comanda` INT(11) NOT NULL,
+  `producto_hamburguesa_id_producto_hamburguesa` INT(10) UNSIGNED NOT NULL,
+  `cantidad` INT(2) NULL DEFAULT NULL,
+  PRIMARY KEY (`comanda_id_comanda`, `producto_hamburguesa_id_producto_hamburguesa`),
+  INDEX `fk_comanda_has_producto_hamburguesa_producto_hamburguesa1_idx` (`producto_hamburguesa_id_producto_hamburguesa` ASC) ,
+  INDEX `fk_comanda_has_producto_hamburguesa_comanda1_idx` (`comanda_id_comanda` ASC) ,
+  CONSTRAINT `fk_comanda_has_producto_hamburguesa_comanda1`
+    FOREIGN KEY (`comanda_id_comanda`)
+    REFERENCES `pizzeria`.`comanda` (`id_comanda`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comanda_has_producto_hamburguesa_producto_hamburguesa1`
+    FOREIGN KEY (`producto_hamburguesa_id_producto_hamburguesa`)
+    REFERENCES `pizzeria`.`producto_hamburguesa` (`id_producto_hamburguesa`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`producto_pizza`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pizzeria`.`producto_pizza` (
+  `id_producto_pizza` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(30) NOT NULL,
+  `descripcion` MEDIUMTEXT NOT NULL,
+  `imagen` VARCHAR(20) NOT NULL,
+  `precio` DOUBLE NOT NULL,
+  `categoria_pizza_id_categoria_pizza` INT(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id_producto_pizza`, `categoria_pizza_id_categoria_pizza`),
+  INDEX `fk_producto_pizza_categoria_pizza1_idx` (`categoria_pizza_id_categoria_pizza` ASC) ,
+  CONSTRAINT `fk_producto_pizza_categoria_pizza1`
+    FOREIGN KEY (`categoria_pizza_id_categoria_pizza`)
+    REFERENCES `pizzeria`.`categoria_pizza` (`id_categoria_pizza`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `pizzeria`.`comanda_has_producto_pizza`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pizzeria`.`comanda_has_producto_pizza` (
+  `comanda_id_comanda` INT(11) NOT NULL,
+  `producto_pizza_id_producto_pizza` INT(10) UNSIGNED NOT NULL,
+  `cantidad` INT(2) NULL DEFAULT NULL,
+  PRIMARY KEY (`comanda_id_comanda`, `producto_pizza_id_producto_pizza`),
+  INDEX `fk_comanda_has_producto_pizza_producto_pizza1_idx` (`producto_pizza_id_producto_pizza` ASC) ,
+  INDEX `fk_comanda_has_producto_pizza_comanda1_idx` (`comanda_id_comanda` ASC) ,
+  CONSTRAINT `fk_comanda_has_producto_pizza_comanda1`
+    FOREIGN KEY (`comanda_id_comanda`)
+    REFERENCES `pizzeria`.`comanda` (`id_comanda`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comanda_has_producto_pizza_producto_pizza1`
+    FOREIGN KEY (`producto_pizza_id_producto_pizza`)
+    REFERENCES `pizzeria`.`producto_pizza` (`id_producto_pizza`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+USE `optica` ;
 
 -- -----------------------------------------------------
 -- Table `optica`.`direccion`
@@ -15,12 +321,12 @@ CREATE SCHEMA IF NOT EXISTS `optica` DEFAULT CHARACTER SET utf8 ;
 CREATE TABLE IF NOT EXISTS `optica`.`direccion` (
   `id_direccion` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `calle` VARCHAR(20) NOT NULL,
-  `numero` INT(4) NOT NULL,
+  `numero` VARCHAR(12) NOT NULL,
   `piso` VARCHAR(10) NOT NULL,
   `puerta` VARCHAR(10) NOT NULL,
   `ciudad` VARCHAR(15) NOT NULL,
-  `codigo_postal` INT(5) NOT NULL,
-  `pais` VARCHAR(15) NULL DEFAULT 'Espanya',
+  `codigo_postal` VARCHAR(6) NOT NULL,
+  `pais` VARCHAR(56) NULL DEFAULT 'Espanya',
   PRIMARY KEY (`id_direccion`))
 ENGINE = InnoDB
 AUTO_INCREMENT = 4
@@ -34,7 +340,7 @@ CREATE TABLE IF NOT EXISTS `optica`.`clientes` (
   `id_clientes` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(20) NOT NULL,
   `direccion_id_direccion` INT(10) UNSIGNED NOT NULL,
-  `telefono` INT(9) NOT NULL,
+  `telefono` VARCHAR(12) NOT NULL,
   `correo_electronico` VARCHAR(45) NOT NULL,
   `fecha_registro` DATE NOT NULL,
   `recomendado_id_clientes` INT(10) UNSIGNED NULL DEFAULT NULL,
@@ -63,8 +369,8 @@ CREATE TABLE IF NOT EXISTS `optica`.`proveedor` (
   `id_proveedor` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(45) NOT NULL,
   `direccion_id_direccion` INT(10) UNSIGNED NOT NULL,
-  `telefono` INT(9) NOT NULL,
-  `fax` INT(9) NULL DEFAULT NULL,
+  `telefono` VARCHAR(12) NOT NULL,
+  `fax` VARCHAR(12) NULL DEFAULT NULL,
   `nif` VARCHAR(9) NOT NULL,
   PRIMARY KEY (`id_proveedor`),
   INDEX `fk_proveedor_direccion1_idx` (`direccion_id_direccion` ASC) ,
@@ -103,16 +409,28 @@ DEFAULT CHARACTER SET = utf8;
 CREATE TABLE IF NOT EXISTS `optica`.`gafas` (
   `id_gafas` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `marca_id_marca` INT(10) UNSIGNED NOT NULL,
-  `graduacion` VARCHAR(45) NOT NULL,
   `tipo_montura` ENUM('flotante', 'pasta', 'metalica') NOT NULL,
   `color_montura` VARCHAR(45) NOT NULL,
-  `color_vidrio` VARCHAR(10) NOT NULL,
   `precio` DOUBLE NOT NULL,
+  `lente_izquierda_id_lente_izquierda` INT NOT NULL,
+  `lente_derecha_id_lente_derecha` INT NOT NULL,
   PRIMARY KEY (`id_gafas`),
   INDEX `fk_gafas_marca1_idx` (`marca_id_marca` ASC) ,
+  INDEX `fk_gafas_lente_izquierda1_idx` (`lente_izquierda_id_lente_izquierda` ASC) ,
+  INDEX `fk_gafas_lente_derecha1_idx` (`lente_derecha_id_lente_derecha` ASC) ,
   CONSTRAINT `fk_gafas_marca1`
     FOREIGN KEY (`marca_id_marca`)
     REFERENCES `optica`.`marca` (`id_marca`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_gafas_lente_izquierda1`
+    FOREIGN KEY (`lente_izquierda_id_lente_izquierda`)
+    REFERENCES `mydb`.`lente_izquierda` (`id_lente_izquierda`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_gafas_lente_derecha1`
+    FOREIGN KEY (`lente_derecha_id_lente_derecha`)
+    REFERENCES `mydb`.`lente_derecha` (`id_lente_derecha`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -168,37 +486,3 @@ DEFAULT CHARACTER SET = utf8;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
-
--- -----------------------------------------------------
--- Agregar valores a las tablas
--- -----------------------------------------------------
-INSERT INTO `direccion` VALUES ('1', 'Peru', '32', '5', '1', 'Barcelona', '08031', NULL);
-INSERT INTO `direccion` VALUES ('2', 'Llull', '122', '2', '9', 'Barcelona', '08031', NULL);
-INSERT INTO `direccion` VALUES ('3', 'Almogavers', '44', '4', '1', 'Barcelona', '08031', NULL);
-
-INSERT INTO `proveedor` VALUES('1','Progafas.SL','1', 934543250,934543250, 29268166);
-INSERT INTO `proveedor` VALUES('2','GrupoLFT.SL','2', 934545451,934526252, 29679166);
-INSERT INTO `proveedor` VALUES('3','MayoristaLK.SL','3', 933345451,934526214, 29679114);
-
-INSERT INTO `trabajador` VALUES ('1', 'Jose Luis Perez');
-INSERT INTO `trabajador` VALUES ('2', 'Sebastian Lopez');
-INSERT INTO `trabajador` VALUES ('3', 'Sara Martinez');
-
-INSERT INTO `clientes` VALUES ('1','Andres', 1, 934191321,'correoandres@correo.com,',"2021-11-06", 1);
-INSERT INTO `clientes` VALUES ('2','Paula', 2 , 934161721,'correopaula@correo.com,',"2021-04-03", 1);
-INSERT INTO `clientes` VALUES ('3','Sara', 3, 934543242, 'correosara@correo.com',"2021-04-03", 1);
-
-INSERT INTO `marca` VALUES('1', 'Rayban',1);
-INSERT INTO `marca` VALUES('2', 'Police',1);
-INSERT INTO `marca` VALUES('3', 'Carlolina Herrera',1);
-
-INSERT INTO `gafas` VALUES('1', 1, '+2,25, -1,75, + 4,50, -0,75', 'pasta', 'azul', 'rojo',145);
-INSERT INTO `gafas` VALUES('2', 2, '+2,25, -1,75, + 4,50, -0,75', 'pasta', 'verde', 'negro',214);
-INSERT INTO `gafas` VALUES('3', 2, '+1,25, -0,75, + 2,50, -0,77', 'pasta', 'negro', 'negro',185);
-
-INSERT INTO `ventas` VALUES (1,"2022-06-14",1,1,1);
-INSERT INTO `ventas` VALUES (2,"2022-06-14",2,2,3);
-INSERT INTO `ventas` VALUES (3,"2022-02-11",3,3,2);
-INSERT INTO `ventas` VALUES (4,"2022-05-12",3,3,1);
-
